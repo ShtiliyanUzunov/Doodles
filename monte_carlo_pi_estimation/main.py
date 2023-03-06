b0ab4fd7b7f22c4b1a5c624571a43648f9abdb67
Shtiliyan Uzunov <shtiliyanuzunov@gmail.com>
2023-03-06 20:41:44 +0200

    Montecarlo pi estimation

---
+++ monte_carlo_pi_estimation/main.py
@@ -0,0 +1,16 @@
+import math
+import random
+
+sample_size = 100000
+
+distances = []
+
+for i in range(0, sample_size):
+    x = random.random()
+    y = random.random()
+    distance = math.sqrt(x * x + y * y)
+    distances.append(distance >= 1)
+
+distances_true = [t for t in distances if t]
+distances_false = [t for t in distances if not t]
+print(f'PI approximation: {len(distances_false) / len(distances) * 4}')
